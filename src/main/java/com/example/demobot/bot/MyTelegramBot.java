a7efb819f6686d18353525594a44ce7fa737e43c
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-08-15 20:47:31 +0300

    added reply keyboard button

--- src/main/java/com/example/demobot/bot/MyTelegramBot.java
+++ src/main/java/com/example/demobot/bot/MyTelegramBot.java
@@ -1,6 +1,7 @@
 package com.example.demobot.bot;
 
 import com.example.demobot.bot.constants.Actions;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
@@ -8,6 +9,9 @@ import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 /**
@@ -37,8 +41,18 @@ public class MyTelegramBot extends TelegramLongPollingCommandBot {
       switch (callbackQuery.getData()) {
         case Actions.SOME_ACTION -> {
           try {
-            sendApiMethod(new SendMessage(callbackQuery.getMessage().getChatId().toString(),
-                "After callback message"));
+            ReplyKeyboardMarkup replyKeyboardMarkup = ReplyKeyboardMarkup.builder()
+                .resizeKeyboard(true)
+                .keyboardRow(new KeyboardRow(
+                    List.of(
+                        new KeyboardButton("I'm reply keyboard button")
+                    )
+                ))
+                .build();
+            sendApiMethod(SendMessage.builder().chatId(callbackQuery.getMessage().getChatId())
+                .text("After callback message")
+                .replyMarkup(replyKeyboardMarkup)
+                .build());
           } catch (TelegramApiException e) {
             throw new RuntimeException(e);
           }

af89243bb3ff5f1e4547e1cedddc7e5ec3d673b7
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-08-15 20:32:25 +0300

    added an InlineKeyboard button with reference to webinar

--- src/main/java/com/example/demobot/bot/commands/StartCommandHandler.java
+++ src/main/java/com/example/demobot/bot/commands/StartCommandHandler.java
@@ -1,11 +1,14 @@
 package com.example.demobot.bot.commands;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -22,7 +25,23 @@ public class StartCommandHandler extends BotCommand {
   @Override
   public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
     try {
-      absSender.execute(new SendMessage(chat.getId().toString(),"Hello world"));
+      InlineKeyboardMarkup inlineKeyboardMarkup = InlineKeyboardMarkup.builder()
+          .keyboardRow(List.of(
+              InlineKeyboardButton.builder()
+                  .text("I'm a link to youtube channel")
+                  .url("https://www.youtube.com/watch?v=lQ-_F2NUAiE&ab_channel=CodeUA")
+                  .build()
+              )
+
+          )
+          .build();
+
+      SendMessage sendMessage = SendMessage.builder()
+          .chatId(chat.getId())
+          .text("Some text")
+          .replyMarkup(inlineKeyboardMarkup)
+          .build();
+      absSender.execute(sendMessage);
     } catch (TelegramApiException e) {
       throw new RuntimeException(e);
     }
